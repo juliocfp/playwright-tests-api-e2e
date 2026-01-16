dfcf6aa3a5e6fb92c8a809c442ab2cd0f94f5561
Julio Parra <julio1652@gmail.com>
2026-01-16 01:16:35 -0300

    test: replace assert statements with Playwright's expect for better readability and consistency

--- api-tests/utils/helpers.py
+++ api-tests/utils/helpers.py
@@ -1,8 +1,8 @@
-from playwright.sync_api import APIRequestContext, APIResponse
+from playwright.sync_api import APIRequestContext, APIResponse, expect
 from utils.factories import get_user_payload
 
 def create_user_helper(api_gorest: APIRequestContext) -> APIResponse:
     """Helper para criar um usuário e retornar o response."""
     response = api_gorest.post("/public/v2/users", data=get_user_payload())
-    assert response.ok
+    expect(response).to_be_ok()
     return response
